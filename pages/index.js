d34491aba6fe9a7242364a4c4a51be919f8c1fa9
MohammadBohlak <abuomarcom4@gmail.com>
2024-10-04 03:20:25 +0300

    s

--- pages/index.js
+++ pages/index.js
@@ -2,49 +2,13 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   
+  const [name, setName] = useState("");
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
   return (
     <>
-      <form>
-        <div>
-          <label>الاسم :</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label>التاريخ :</label>
-          <div>
-            <label>اليوم :</label>
-            <input
-              type="text"
-              value={day}
-              onChange={(e) => {
-                setDay(e.target.value);
-              }}
-            />
-            <label>الشهر :</label>
-            <input
-              type="text"
-              value={month}
-              onChange={(e) => {
-                setMonth(e.target.value);
-              }}
-            />
-            <label>السنة :</label>
-            <input
-              type="text"
-              value={year}
-              onChange={(e) => {
-                setYear(e.target.value);
-              }}
-            />
-          </div>
-        </div>
-      </form>
+    hello my gym
     </>
   );
 }
